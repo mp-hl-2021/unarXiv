a32929d145610863a139659d8c1483404849841a
Egor Gorbachev <peltorator@yandex.ru>
2021-03-15 12:10:07 +0300

    Change tabs to spaces in api/ and core/

--- api/unarXiv.go
+++ api/unarXiv.go
@@ -1,128 +1,128 @@
 package api
 
 type UnarXiv interface {
-	Register(*AuthenticationRequest) (*AuthenticationData, error)
-	Login(*AuthenticationRequest) (*AuthenticationData, error)
+    Register(*AuthenticationRequest) (*AuthenticationData, error)
+    Login(*AuthenticationRequest) (*AuthenticationData, error)
 
-	Search(*SearchQueryRequest) (*SearchQueryResponse, error)
+    Search(*SearchQueryRequest) (*SearchQueryResponse, error)
 
-	SetArticleSubscriptionStatus(*SetArticleSubscriptionStatusRequest) (*ArticleUserMetaInfo, error)
-	GetArticleSubscriptionStatus(*GetArticleSubscriptionStatusRequest) (*ArticleUserMetaInfo, error)
-	SetSearchQuerySubscriptionStatus(*SetSearchQuerySubscriptionStatusRequest) (*SearchQueryUserMetaInfo, error)
-	GetSearchQuerySubscriptionStatus(*GetSearchQuerySubscriptionStatusRequest) (*SearchQueryUserMetaInfo, error)
+    SetArticleSubscriptionStatus(*SetArticleSubscriptionStatusRequest) (*ArticleUserMetaInfo, error)
+    GetArticleSubscriptionStatus(*GetArticleSubscriptionStatusRequest) (*ArticleUserMetaInfo, error)
+    SetSearchQuerySubscriptionStatus(*SetSearchQuerySubscriptionStatusRequest) (*SearchQueryUserMetaInfo, error)
+    GetSearchQuerySubscriptionStatus(*GetSearchQuerySubscriptionStatusRequest) (*SearchQueryUserMetaInfo, error)
 
-	GetArticlesUpdates(*AuthenticationData) (*GetArticlesUpdatesResponse, error)
-	AccessArticle(*AccessArticleRequest) (*AccessArticleResponse, error)
+    GetArticlesUpdates(*AuthenticationData) (*GetArticlesUpdatesResponse, error)
+    AccessArticle(*AccessArticleRequest) (*AccessArticleResponse, error)
 
-	GetSearchQueriesUpdates(*AuthenticationData) (*GetSearchQueriesUpdatesResponse, error)
+    GetSearchQueriesUpdates(*AuthenticationData) (*GetSearchQueriesUpdatesResponse, error)
 
-	GetSearchHistory(*AuthenticationData) (*GetSearchHistoryResponse, error)
-	GetArticlesHistory(*AuthenticationData) (*GetAccessedArticlesHistoryResponse, error)
+    GetSearchHistory(*AuthenticationData) (*GetSearchHistoryResponse, error)
+    GetArticlesHistory(*AuthenticationData) (*GetAccessedArticlesHistoryResponse, error)
 }
 
 type AuthenticationRequest struct {
-	Login    string `json:"login"`
-	Password string `json:"password"`
+    Login    string `json:"login"`
+    Password string `json:"password"`
 }
 
 type AuthenticationData struct {
-	Jwt string `json:"jwt"`
+    Jwt string `json:"jwt"`
 }
 
 type ArticleMetaInfo struct {
-	Title     string `json:"title"`
-	Authors   string `json:"authors"`
-	ArticleId string `json:"articleId"`
-	Abstract  string `json:"abstract"`
+    Title     string `json:"title"`
+    Authors   string `json:"authors"`
+    ArticleId string `json:"articleId"`
+    Abstract  string `json:"abstract"`
 
-	LastUpdateTimestamp uint64 `json:"lastUpdateTimestamp"`
+    LastUpdateTimestamp uint64 `json:"lastUpdateTimestamp"`
 
-	// this field is present if user is authenticated
-	UserMetaInfo *ArticleUserMetaInfo `json:"userMetaInfo,omitempty"`
+    // this field is present if user is authenticated
+    UserMetaInfo *ArticleUserMetaInfo `json:"userMetaInfo,omitempty"`
 }
 
 type ArticleUserMetaInfo struct {
-	IsSubscribed          bool   `json:"isSubscribed"`
-	LastAccessedTimestamp uint64 `json:"lastAccessedTimestamp"`
+    IsSubscribed          bool   `json:"isSubscribed"`
+    LastAccessedTimestamp uint64 `json:"lastAccessedTimestamp"`
 }
 
 type SearchQueryRequest struct {
-	Query    string              `json:"query"`
-	Offset   *uint32             `json:"offset,omitempty"`
-	AuthData *AuthenticationData `json:"authData,omitempty"`
+    Query    string              `json:"query"`
+    Offset   *uint32             `json:"offset,omitempty"`
+    AuthData *AuthenticationData `json:"authData,omitempty"`
 }
 
 type SearchQueryResponse struct {
-	TotalMatchesCount uint32                    `json:"totalMatchesCount"`
-	Matches           []SearchQueryResponseItem `json:"matches"`
-	UserMetaInfo      *SearchQueryUserMetaInfo  `json:"userMetaInfo,omitempty"`
+    TotalMatchesCount uint32                    `json:"totalMatchesCount"`
+    Matches           []SearchQueryResponseItem `json:"matches"`
+    UserMetaInfo      *SearchQueryUserMetaInfo  `json:"userMetaInfo,omitempty"`
 }
 
 type SearchQueryUserMetaInfo struct {
-	IsSubscribed          bool   `json:"isSubscribed"`
-	LastAccessedTimestamp uint64 `json:"lastAccessedTimestamp"`
+    IsSubscribed          bool   `json:"isSubscribed"`
+    LastAccessedTimestamp uint64 `json:"lastAccessedTimestamp"`
 }
 
 type SearchQueryResponseItem struct {
-	Article ArticleMetaInfo `json:"article"`
+    Article ArticleMetaInfo `json:"article"`
 }
 
 type SetArticleSubscriptionStatusRequest struct {
-	AuthData  AuthenticationData `json:"authData"`
-	ArticleId string             `json:"articleId"`
-	Subscribe bool               `json:"subscribe"`
+    AuthData  AuthenticationData `json:"authData"`
+    ArticleId string             `json:"articleId"`
+    Subscribe bool               `json:"subscribe"`
 }
 
 type GetArticleSubscriptionStatusRequest struct {
-	AuthData  AuthenticationData `json:"authData"`
-	ArticleId string             `json:"articleId"`
+    AuthData  AuthenticationData `json:"authData"`
+    ArticleId string             `json:"articleId"`
 }
 
 type SetSearchQuerySubscriptionStatusRequest struct {
-	AuthData  AuthenticationData `json:"authData"`
-	Query     string             `json:"query"`
-	Subscribe bool               `json:"subscribe"`
+    AuthData  AuthenticationData `json:"authData"`
+    Query     string             `json:"query"`
+    Subscribe bool               `json:"subscribe"`
 }
 
 type GetSearchQuerySubscriptionStatusRequest struct {
-	AuthData AuthenticationData `json:"authData"`
-	Query    string             `json:"query"`
+    AuthData AuthenticationData `json:"authData"`
+    Query    string             `json:"query"`
 }
 
 type GetArticlesUpdatesResponse struct {
-	UpdatedArticles []ArticleMetaInfo `json:"updatedArticles"`
+    UpdatedArticles []ArticleMetaInfo `json:"updatedArticles"`
 }
 
 type AccessArticleRequest struct {
-	AuthData  *AuthenticationData `json:"authData,omitempty"`
-	ArticleId string             `json:"articleId"`
+    AuthData  *AuthenticationData `json:"authData,omitempty"`
+    ArticleId string             `json:"articleId"`
 }
 
 type AccessArticleResponse struct {
-	Article ArticleMetaInfo `json:"article"`
+    Article ArticleMetaInfo `json:"article"`
 }
 
 type GetSearchQueriesUpdatesResponse struct {
-	SearchQueriesUpdates []GetSearchQueriesUpdatesResponseItem `json:"searchQueriesUpdates"`
+    SearchQueriesUpdates []GetSearchQueriesUpdatesResponseItem `json:"searchQueriesUpdates"`
 }
 
 type GetSearchQueriesUpdatesResponseItem struct {
-	Query string `json:"query"`
-	// presents if there's at least 1 article that matches the query
-	LastNewArticleTimestamp *uint64 `json:"lastNewArticleTimestamp,omitempty"`
-	LastAccessedTimestamp   uint64  `json:"lastAccessedTimestamp"`
-	NewArticlesCount        *uint32 `json:"newArticlesCount,omitempty"`
+    Query string `json:"query"`
+    // presents if there's at least 1 article that matches the query
+    LastNewArticleTimestamp *uint64 `json:"lastNewArticleTimestamp,omitempty"`
+    LastAccessedTimestamp   uint64  `json:"lastAccessedTimestamp"`
+    NewArticlesCount        *uint32 `json:"newArticlesCount,omitempty"`
 }
 
 type GetSearchHistoryResponse struct {
-	SearchHistory []GetSearchHistoryResponseItem `json:"searchHistory"`
+    SearchHistory []GetSearchHistoryResponseItem `json:"searchHistory"`
 }
 
 type GetSearchHistoryResponseItem struct {
-	Query        string                  `json:"query"`
-	UserMetaInfo SearchQueryUserMetaInfo `json:"userMetaInfo"`
+    Query        string                  `json:"query"`
+    UserMetaInfo SearchQueryUserMetaInfo `json:"userMetaInfo"`
 }
 
 type GetAccessedArticlesHistoryResponse struct {
-	AccessedArticlesHistory []ArticleMetaInfo `json:"accessedArticlesHistory"`
+    AccessedArticlesHistory []ArticleMetaInfo `json:"accessedArticlesHistory"`
 }
--- core/dummyUnarXiv.go
+++ core/dummyUnarXiv.go
@@ -6,137 +6,137 @@ var dummyUint32 uint32 = 0
 var dummyUint64 uint64 = 0
 
 var dummyAuthenticationRequest = api.AuthenticationRequest{
-	Login:    "login",
-	Password: "password",
+    Login:    "login",
+    Password: "password",
 }
 
 var dummyAuthenticationData = api.AuthenticationData{
-	Jwt: "jwt",
+    Jwt: "jwt",
 }
 
 var dummyArticleUserMetaInfo = api.ArticleUserMetaInfo{
-	IsSubscribed: true,
+    IsSubscribed: true,
 }
 
 var dummyArticleMetaInfo = api.ArticleMetaInfo{
-	Title:        "title",
-	Authors:      "authors",
-	ArticleId:    "articleId",
-	Abstract:     "abstract",
-	UserMetaInfo: &dummyArticleUserMetaInfo,
+    Title:        "title",
+    Authors:      "authors",
+    ArticleId:    "articleId",
+    Abstract:     "abstract",
+    UserMetaInfo: &dummyArticleUserMetaInfo,
 }
 
 var dummySearchQueryRequest = api.SearchQueryRequest{
-	Query:    "query",
-	Offset:   &dummyUint32,
-	AuthData: &dummyAuthenticationData,
+    Query:    "query",
+    Offset:   &dummyUint32,
+    AuthData: &dummyAuthenticationData,
 }
 
 var dummySearchQueryResponseItem = api.SearchQueryResponseItem{
-	Article: dummyArticleMetaInfo,
+    Article: dummyArticleMetaInfo,
 }
 
 var dummySearchQueryUserMetaInfo = api.SearchQueryUserMetaInfo{
-	IsSubscribed: true,
+    IsSubscribed: true,
 }
 
 var dummySearchQueryResponse = api.SearchQueryResponse{
-	TotalMatchesCount: 1,
-	Matches:           []api.SearchQueryResponseItem{dummySearchQueryResponseItem},
-	UserMetaInfo:      &dummySearchQueryUserMetaInfo,
+    TotalMatchesCount: 1,
+    Matches:           []api.SearchQueryResponseItem{dummySearchQueryResponseItem},
+    UserMetaInfo:      &dummySearchQueryUserMetaInfo,
 }
 
 var dummyGetArticleSubscriptionStatusRequest = api.GetArticleSubscriptionStatusRequest{
-	AuthData:  dummyAuthenticationData,
-	ArticleId: "articleId",
+    AuthData:  dummyAuthenticationData,
+    ArticleId: "articleId",
 }
 
 var dummySetSearchQuerySubscriptionStatusRequest = api.SetSearchQuerySubscriptionStatusRequest{
-	AuthData:  dummyAuthenticationData,
-	Query:     "query",
-	Subscribe: true,
+    AuthData:  dummyAuthenticationData,
+    Query:     "query",
+    Subscribe: true,
 }
 
 var dummyGetSearchQuerySubscriptionStatusRequest = api.GetSearchQuerySubscriptionStatusRequest{
-	AuthData: dummyAuthenticationData,
-	Query:    "query",
+    AuthData: dummyAuthenticationData,
+    Query:    "query",
 }
 
 var dummyGetArticlesUpdatesResponse = api.GetArticlesUpdatesResponse{
-	UpdatedArticles: []api.ArticleMetaInfo{dummyArticleMetaInfo},
+    UpdatedArticles: []api.ArticleMetaInfo{dummyArticleMetaInfo},
 }
 
 var dummyAccessArticleRequest = api.AccessArticleRequest{
-	AuthData:  &dummyAuthenticationData,
-	ArticleId: "articleId",
+    AuthData:  &dummyAuthenticationData,
+    ArticleId: "articleId",
 }
 
 var dummyAccessArticleResponse = api.AccessArticleResponse{
-	Article: dummyArticleMetaInfo,
+    Article: dummyArticleMetaInfo,
 }
 
 var dummyGetSearchQueriesUpdatesResponse = api.GetSearchQueriesUpdatesResponse{
-	SearchQueriesUpdates: []api.GetSearchQueriesUpdatesResponseItem{dummyGetSearchQueriesUpdatesResponseItem},
+    SearchQueriesUpdates: []api.GetSearchQueriesUpdatesResponseItem{dummyGetSearchQueriesUpdatesResponseItem},
 }
 
 var dummyGetSearchQueriesUpdatesResponseItem = api.GetSearchQueriesUpdatesResponseItem{
-	Query:                   "query",
-	LastNewArticleTimestamp: &dummyUint64,
-	NewArticlesCount:        &dummyUint32,
+    Query:                   "query",
+    LastNewArticleTimestamp: &dummyUint64,
+    NewArticlesCount:        &dummyUint32,
 }
 
 var dummyGetSearchHistoryResponse = api.GetSearchHistoryResponse{
-	SearchHistory: []api.GetSearchHistoryResponseItem{dummyGetSearchHistoryResponseItem},
+    SearchHistory: []api.GetSearchHistoryResponseItem{dummyGetSearchHistoryResponseItem},
 }
 
 var dummyGetSearchHistoryResponseItem = api.GetSearchHistoryResponseItem{
-	Query:        "query",
-	UserMetaInfo: dummySearchQueryUserMetaInfo,
+    Query:        "query",
+    UserMetaInfo: dummySearchQueryUserMetaInfo,
 }
 
 var dummyGetAccessedArticlesHistoryResponse = api.GetAccessedArticlesHistoryResponse{
-	AccessedArticlesHistory: []api.ArticleMetaInfo{dummyArticleMetaInfo},
+    AccessedArticlesHistory: []api.ArticleMetaInfo{dummyArticleMetaInfo},
 }
 
 type DummyUnarXivAPI struct{}
 
 func (DummyUnarXivAPI) Register(*api.AuthenticationRequest) (*api.AuthenticationData, error) {
-	return &dummyAuthenticationData, nil
+    return &dummyAuthenticationData, nil
 }
 func (DummyUnarXivAPI) Login(*api.AuthenticationRequest) (*api.AuthenticationData, error) {
-	return &dummyAuthenticationData, nil
+    return &dummyAuthenticationData, nil
 }
 func (DummyUnarXivAPI) Search(*api.SearchQueryRequest) (*api.SearchQueryResponse, error) {
-	return &dummySearchQueryResponse, nil
+    return &dummySearchQueryResponse, nil
 }
 
 func (DummyUnarXivAPI) SetArticleSubscriptionStatus(*api.SetArticleSubscriptionStatusRequest) (*api.ArticleUserMetaInfo, error) {
-	return &dummyArticleUserMetaInfo, nil
+    return &dummyArticleUserMetaInfo, nil
 }
 func (DummyUnarXivAPI) GetArticleSubscriptionStatus(*api.GetArticleSubscriptionStatusRequest) (*api.ArticleUserMetaInfo, error) {
-	return &dummyArticleUserMetaInfo, nil
+    return &dummyArticleUserMetaInfo, nil
 }
 func (DummyUnarXivAPI) SetSearchQuerySubscriptionStatus(*api.SetSearchQuerySubscriptionStatusRequest) (*api.SearchQueryUserMetaInfo, error) {
-	return &dummySearchQueryUserMetaInfo, nil
+    return &dummySearchQueryUserMetaInfo, nil
 }
 func (DummyUnarXivAPI) GetSearchQuerySubscriptionStatus(*api.GetSearchQuerySubscriptionStatusRequest) (*api.SearchQueryUserMetaInfo, error) {
-	return &dummySearchQueryUserMetaInfo, nil
+    return &dummySearchQueryUserMetaInfo, nil
 }
 
 func (DummyUnarXivAPI) GetArticlesUpdates(*api.AuthenticationData) (*api.GetArticlesUpdatesResponse, error) {
-	return &dummyGetArticlesUpdatesResponse, nil
+    return &dummyGetArticlesUpdatesResponse, nil
 }
 func (DummyUnarXivAPI) AccessArticle(*api.AccessArticleRequest) (*api.AccessArticleResponse, error) {
-	return &dummyAccessArticleResponse, nil
+    return &dummyAccessArticleResponse, nil
 }
 
 func (DummyUnarXivAPI) GetSearchQueriesUpdates(*api.AuthenticationData) (*api.GetSearchQueriesUpdatesResponse, error) {
-	return &dummyGetSearchQueriesUpdatesResponse, nil
+    return &dummyGetSearchQueriesUpdatesResponse, nil
 }
 
 func (DummyUnarXivAPI) GetSearchHistory(*api.AuthenticationData) (*api.GetSearchHistoryResponse, error) {
-	return &dummyGetSearchHistoryResponse, nil
+    return &dummyGetSearchHistoryResponse, nil
 }
 func (DummyUnarXivAPI) GetArticlesHistory(*api.AuthenticationData) (*api.GetAccessedArticlesHistoryResponse, error) {
-	return &dummyGetAccessedArticlesHistoryResponse, nil
+    return &dummyGetAccessedArticlesHistoryResponse, nil
 }
