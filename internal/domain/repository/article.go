1b06e05ee093cd9ec90588d73c5889d02bf161c5
Vadim Salavatov <vmsalavatov@gmail.com>
2021-03-30 00:36:09 +0300

    ->DDD: +domain/repos

---
+++ internal/domain/repository/article.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+    "github.com/mp-hl-2021/unarXiv/internal/domain/model"
+)
+
+type ArticleRepo interface {
+    ArticleMetaById(id model.ArticleId) (model.ArticleMeta, error)
+    ArticleById(id model.ArticleId) (model.Article, error)
+
+    UpdateArticle(article model.Article)
+
+    Search(query model.SearchQuery, limit uint32) (model.SearchResult, error)
+}
