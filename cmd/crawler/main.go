a683a0cc8dbddd002d7e3cb47d25ed868768ddc9
peltorator <peltorator@gmail.com>
2021-04-17 11:18:40 +0300

    Improve readability

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -8,48 +8,64 @@ import (
     "time"
 
     "github.com/gocolly/colly/v2"
+    "github.com/tkanos/gonfig"
 )
 
-func main() {
-    fmt.Println("Go go yees")
+type CrawlerConfiguration struct {
+    CrawlDomain      string
+    CrawlStart       string
+    LocalStoragePath string
+    NeedArticles     int
+}
+
+func GetCrawlerConfiguration(cfgPath string) CrawlerConfiguration {
+    cfg := CrawlerConfiguration{}
+    err := gonfig.GetConf(cfgPath, &cfg)
+    if err != nil {
+        panic(err)
+    }
+    return cfg
+}
+
+func CrawlArticles(cfg CrawlerConfiguration) []string {
+    fmt.Println("Start crawling")
     c := colly.NewCollector()
 
     used := make(map[string]bool)
-    urlQueue := make([]string, 0)
+    urlQueue := make(chan string, 1000)
     articles := make([]string, 0)
 
     c.OnHTML("a[href]", func(e *colly.HTMLElement) {
         url := e.Attr("href")
-        if !used[url] && strings.Contains(url, "arxiv.org") {
+        if !used[url] && strings.Contains(url, cfg.CrawlDomain) {
             if strings.Contains(url, "abs") {
                 articles = append(articles, url)
             }
             used[url] = true
-            urlQueue = append(urlQueue, url)
+            urlQueue <- url
         }
 	})
-
     c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-    urlQueue = append(urlQueue, "https://arxiv.org/covid19search")
 
-    needArticles := 10
-
-    for len(articles) < needArticles && len(urlQueue) > 0 {
-        url := urlQueue[0]
-        urlQueue = urlQueue[1:]
-        c.Visit(url)
+    urlQueue <- cfg.CrawlStart
+    for len(articles) < cfg.NeedArticles && len(urlQueue) > 0 {
+        c.Visit(<-urlQueue)
     }
 
-    articles = articles[:needArticles]
+    return articles[:cfg.NeedArticles]
+}
 
-    for _, s := range articles {
-        fmt.Println("Found article", s)
-    }
+func GetFieldFromHTML(c *colly.Collector, saveTo *string, className string) {
+    c.OnHTML(fmt.Sprintf("[class=\"%s\"]", className), func (e *colly.HTMLElement) {
+        *saveTo = strings.Replace(e.Text, "\n", " ", -1)
+    })
+}
 
-    f, err := os.Create("data.txt")
+func SaveArticlesInfo(articles []string, cfg CrawlerConfiguration) {
+    f, err := os.Create(cfg.LocalStoragePath)
     if err != nil {
         panic(err)
     }
@@ -57,25 +73,29 @@ func main() {
     w := bufio.NewWriter(f)
 
     fmt.Fprintf(w, "Id;Title;Authors;Abstract;LastUpdateTimestamp\n")
-
     for i, url := range articles {
-        fmt.Println("Checking article number", i)
+        fmt.Println("Saving info about article number", i)
         c2 := colly.NewCollector()
         authors := ""
-        c2.OnHTML(".authors", func(e *colly.HTMLElement) {
-            authors = e.Text
-        })
+        GetFieldFromHTML(c2, &authors, "authors")
         title := ""
-        c2.OnHTML("[class=\"title mathjax\"]", func(e *colly.HTMLElement) {
-            title = e.Text
-        })
+        GetFieldFromHTML(c2, &title, "title mathjax")
         abstract := ""
-        c2.OnHTML("[class=\"abstract mathjax\"]", func(e *colly.HTMLElement) {
-            abstract = strings.Replace(e.Text, "\n", " ", -1)
-        })
+        GetFieldFromHTML(c2, &abstract, "abstract mathjax")
 
         c2.Visit(url)
         fmt.Fprintf(w, "%s;%s;%s;%s;%s\n", url, title, authors, abstract, time.Now().String())
     }
 }
 
+func main() {
+    cfg := GetCrawlerConfiguration("cmd/crawler/crawlercfg.json")
+
+    articles := CrawlArticles(cfg)
+
+    for _, s := range articles {
+        fmt.Println("Found article", s)
+    }
+
+    SaveArticlesInfo(articles, cfg)
+}
